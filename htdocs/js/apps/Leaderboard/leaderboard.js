57098dd9b6f9af4e34b460bc2bfa51b661b28dfd
Michael Gage <1203580+mgage@users.noreply.github.com>
2019-03-14 11:17:52 -0400

    update leaderboard.js.    hardcoded passwords in leaderboard.php not uploaded on purpose

--- htdocs/js/apps/Leaderboard/leaderboard.js
+++ htdocs/js/apps/Leaderboard/leaderboard.js
@@ -9,8 +9,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 // Development file only.
-var user = null;
-var key = null;
+var user = '';
+var key = '';
 
 // get static values from webwork
 var courseName = document.getElementById("courseName").value;

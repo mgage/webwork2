8841e84c9059ff159ef60ae67ab2abf4fa7d0b7c
Michael Gage <1203580+mgage@users.noreply.github.com>
2020-07-31 18:47:22 -0400

    commit final adjustments for JWT

--- lib/WebworkClient/libretexts_format.pl
+++ lib/WebworkClient/libretexts_format.pl
@@ -45,9 +45,6 @@ $problemHeadText
 $answerTemplate
 </div>
 
-<p>
-jwt:$JWTanswerTemplate
-</p>
 
 <script type="text/javascript">window.addEventListener('load',()=>{
 parent.postMessage( {type: 'answerJWT',JWT:'$JWTanswerTemplate'}, "*",);

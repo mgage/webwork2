04eee708bc1a14ed8c8898eedcafb4f15b3ab40f
Michael Gage <1203580+mgage@users.noreply.github.com>
2020-09-22 19:58:04 -0400

    Begin object oriented version of JWT_Utils

--- lib/WeBWorK/Utils/JWT_Utils.pm
+++ lib/WeBWorK/Utils/JWT_Utils.pm
@@ -1,10 +1,20 @@
 
-use base qw(Exporter);
-use JSON;
-use Crypt::JWT qw( decode_jwt encode_jwt);
-use LWP::UserAgent;
-use HTTP::Request;
+package WeBWorK::Utils::JWT_utils;
+use base qw(Class::Accessor);
 
+use 5.10.0;
+use strict;
+use warnings;
+use JSON qw(encode_json decode_json);
+use LWP::UserAgent;
+use Crypt::JWT qw(decode_jwt encode_jwt);
+use Exporter;
+
+use HTTP::Request;
+use lib "/opt/webwork/webwork2/lib";
+
+
+# redo this by exporting an object
 our @EXPORT    = ();
 our @EXPORT_OK = qw(
 	post_to_ADAPT
@@ -15,10 +25,33 @@ our @EXPORT_OK = qw(
 	pp_hash
 );
 
+sub new {
+	my $class = shift;
+	my %options = @_;
+	$class = (ref($class))? ref($class) : $class; # create a new object of the same class
+	my $ce = shift;  # grab a course environment variable
+	my $self = {
+		ce  => $ce,
+		path_to_config_file => '',
+ 		secret_key => 'webwork', # the keyword for signing and the key word for encryption
+		content_alg =>''  , # the algorithm for encrypting the content_alg
+		signing_alg =>'HS256'   , # the algorithm for signing (encrypting the signature)
+		elapsed_time=>0,    # iat to exp
+	};
+	bless $self, $class;
+	# create accessors/mutators
+    $self->mk_ro_accessors (qw( ce path_to_config_file secret_key content_alg signing_alg elapsed_time));
+	_init($self, %options);  #go grab data from the configuration file
+	return $self;
+}
 
 # this is a subroutine not a method
+# perhaps it should be placed in a separate utils file
+sub _init {
+	my $self = shift;
+	$self->{path_to_config_file} = '';
 
-
+}
 sub post_to_ADAPT {
 	my $jwt_to_post = shift;
 	my $ua = LWP::UserAgent->new( 'send_te' => '0' );
@@ -55,18 +88,17 @@ sub post_to_ADAPT {
 }
 
 sub jwt2hash {
-	my %in = @_;
-	my $token = $in{token}//'0';
-	my $key = $in{key}//'webwork';
-	decode_jwt(token=>$token, key=>$key);
-	# FIXME -- make method? get key from $ce?
+	my $self = shift;
+	my $token = shift;
+	$token = $token//'0'; #set default for undefined or give warnings
+	decode_jwt(token=>$token, key=> $self->secret_key );
 }
 
 sub hash2jwt {
-	my %in = @_;
-	my $payload = $in{payload};
-	my $key = $in{key}//'webwork';
-	encode_jwt(payload=>$payload, alg=>'HS256',key=>$key);
+	my $self = shift;
+	my $payload = shift;
+	$payload = $payload//{};  #set default or give warnings
+	encode_jwt(payload=>$payload, alg=> $self->signing_alg ,key => $self->secret_key );
 }
 
 sub json2hash {
@@ -78,8 +110,25 @@ sub hash2json {
 }
 
 sub pp_hash {
-	$perl_hash_ref = shift;
+	my $perl_hash_ref = shift;
 	to_json($perl_hash_ref, {utf8=>1, pretty=>1});
 }
 
+# sub encrypt_jwt_for_ADAPT {
+# 	my $token shift;
+# 	#fetch key and algorithm (needs $ce)
+# 	$key = 
+# 	$alg = 
+# 
+# }
+# 
+# sub decrypt_jwt_for_ADAPT {
+# 	my $payload = shift;
+# 	#fetch key and algorithm (needs $ce)
+# 	$key = 
+# 	$alg = 
+# 
+# 
+# 
+# }
 1;

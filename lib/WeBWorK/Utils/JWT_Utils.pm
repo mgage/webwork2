5e448f01eed680534da6ae7fa78ece3d6799b505
Michael Gage <1203580+mgage@users.noreply.github.com>
2020-10-03 21:17:39 -0400

    Provide friendlier report when the adapt server sends a 400 or 500 error

--- lib/WeBWorK/Utils/JWT_Utils.pm
+++ lib/WeBWorK/Utils/JWT_Utils.pm
@@ -76,16 +76,25 @@ sub post_to_ADAPT {
 	 my $adapt_call_hash = eval{
 						$ua->request( $r, )
 	 };
+	 # model success response
+	 # {"type":"success","message":"Your question submission was saved.",
+	 # "last_submitted":"October 03, 2020 5:54:10pm","student_response":"N\/A"}
 	 if ($@) {
 		warn "problem with curl call $@";
-		return $@;
+		my $current_date = `date`;
+		return qq{ {"type":"failure","message": "unable to contact server-- $@", "last_submitted":"$current_date", "student_response":
+		"N\/A" }"};
 	 }
 	 else { #FIXME make catching error more robust
 	 	if ($adapt_call_hash->{_rc}==200){
 	 		return $adapt_call_hash->{_content}
 	 	}
 	 	else {
-	 		return $adapt_call_hash->{_rc}
+			my $error_code = $adapt_call_hash->{_rc};
+			my $error_msg  = $adapt_call_hash->{_msg};
+			my $current_date = `date`;
+			return qq{ {"type":"failure","message": "server error-- $error_code: $error_msg", "last_submitted":"$current_date" student_response":
+			"N\/A" }"};
 	 	}
 	 }
 # other fields returned in this hash -- possibly useful for debugging
